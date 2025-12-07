5edba816256dca3fc7f8f2dcfdcab6e39ca4b624
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-08 02:09:49 +0530

    Refactor createShort function for consistency

--- controller/user.js
+++ controller/user.js
@@ -1,25 +1,21 @@
 const { nanoid } = require("nanoid");
 const URL = require("../model/url");
 
-module.exports.createShort = async (req, res) => {
-  try {
-    const { redirectURL } = req.body;
-
-    if (!redirectURL) {
+module.exports.createShort = async (req, res)=>{
+  try{
+    const{redirectURL}=req.body;
+    if (!redirectURL){
       return res.status(400).send("URL is required");
     }
-
-    const shortId = nanoid(8); // generates 8-char ID
-
+    const shortId = nanoid(8);
     await URL.create({
       shortId,
       redirectURL,
       visitHistory: [],
     });
-
     return res.redirect("/");
-  } catch (err) {
-    console.error("CreateShort error:", err);
+  }catch (err){
+    console.error("CreateShort error:",err);
     res.status(500).send("Internal Server Error");
   }
 };
